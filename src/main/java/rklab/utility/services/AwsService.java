1340d5d9d77597583e9f00523d2960fd6c6e903f
ritik-agrawal <ritikagrawal1697@gmail.com>
2025-08-16 19:26:08 +0530

    changes

--- src/main/java/rklab/utility/services/AwsService.java
+++ src/main/java/rklab/utility/services/AwsService.java
@@ -12,6 +12,7 @@ import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -53,6 +54,9 @@ public class AwsService {
                                         config.getSecretKey()
                                 )))
                 .region(Region.of(config.getRegion()))
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(config.forcePathStyleUrl())
+                        .build())
                 .build();
     }
 
